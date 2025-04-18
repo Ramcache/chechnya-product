eadb4f4bdc34a4dd44fc123c5a47ab1834578c28
Ramzan <ramaro@internet.ru>
2025-04-19 00:30:17 +0300

    update logger(create directory)

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -3,14 +3,19 @@ package logger
 import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"os"
 )
 
 // NewLogger создаёт новый zap.Logger с выводом в stdout и файл
 func NewLogger() (*zap.Logger, error) {
+	if err := os.MkdirAll("logs", 0755); err != nil {
+		return nil, err
+	}
+
 	cfg := zap.Config{
 		Level:            zap.NewAtomicLevelAt(zap.InfoLevel),
 		Encoding:         "json",
-		OutputPaths:      []string{"stdout", "logs/app.log"}, // лог в консоль и файл
+		OutputPaths:      []string{"stdout", "logs/app.log"},
 		ErrorOutputPaths: []string{"stderr"},
 		EncoderConfig: zapcore.EncoderConfig{
 			TimeKey:      "time",
