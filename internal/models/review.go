f526c273b1e704e5df014697fe46b8686c1a2716
Ramzan <ramaro@internet.ru>
2025-04-25 15:35:11 +0300

    init swagger for review

--- internal/models/review.go
+++ internal/models/review.go
@@ -5,7 +5,11 @@ type Review struct {
 	OwnerID   string `db:"owner_id" json:"owner_id"`
 	ProductID int    `db:"product_id" json:"product_id"`
 	Rating    int    `db:"rating" json:"rating"`
-	Comment   string  `db:"comment" json:"comment"`
-	CreatedAt string  `db:"created_at" json:"created_at"`
+	Comment   string `db:"comment" json:"comment"`
+	CreatedAt string `db:"created_at" json:"created_at"`
 }
 
+type ReviewRequest struct {
+	Rating  int    `json:"rating" example:"5"`
+	Comment string `json:"comment" example:"Отличный товар!"`
+}
